6b21eaad3a09b35f54561aed4b215037bed9b1c1
jexline <joeyexline@gmail.com>
2023-02-16 13:57:55 -0800

    Add files via upload

---
+++ src/main/java/com/exline/bundlesunlocked/BundlesUnlockedMain.java
@@ -0,0 +1,12 @@
+package com.exline.bundlesunlocked;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BundlesUnlockedMain implements ModInitializer {
+
+    public static final String MOD_ID = "bundlesunlocked";
+
+    @Override
+    public void onInitialize() {
+    }
+}
